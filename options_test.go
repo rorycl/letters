cc150cff2c693f071f9988f0cb01901883bd17ab
rorycl <rorycl@campbell-lange.net>
2025-01-11 21:59:50 +0000

    example: add some tests, plug in address header parser to example

--- options_test.go
+++ options_test.go
@@ -1,8 +1,10 @@
 // This is a test showing how ParserOptions could be implemented using
 // the golang functional options pattern.
 //
-// An example blog post on the subject is at
+// A usefulblog post on the subject is at
 // https://davidbacisin.com/writing/golang-options-pattern
+// See also the definitive blog post at
+// https://dave.cheney.net/2014/10/17/functional-options-for-friendly-apis
 //
 // RCL 11 January 2025
 
@@ -89,7 +91,7 @@ So, "Hello".
 		WithSkipAttachments(),
 		WithAddressHeaderFunc(
 			func(mail.Header, string) (*mail.Address, error) {
-				return &mail.Address{"Joe Bloggs", "joe@bloggs.com"}, nil
+				return &mail.Address{Name: "Joe Bloggs", Address: "joe@bloggs.com"}, nil
 			},
 		),
 	)
@@ -105,6 +107,10 @@ So, "Hello".
 		if err != nil {
 			return e, err
 		}
+		e.Headers.From[0], err = parser.parseAddressHeader(email.Header, "From")
+		if err != nil {
+			return e, err
+		}
 		text, err := io.ReadAll(email.Body)
 		if err != nil {
 			return e, err
@@ -113,13 +119,39 @@ So, "Hello".
 		return e, nil
 	}
 
+	// run
 	email, err := parser.Parse(messageReader)
-	fmt.Printf("email: headers:\n%#v\n", email.Headers)
-	fmt.Printf("email: from   :\n%#v\n", email.Headers.From[0])
-	fmt.Printf("email: text    \n%s \n", email.Text)
-	fmt.Printf("email: err:    \n%t \n", err != nil)
 
-	fmt.Printf("parser:        \n%#v\n", parser)
+	// view and validate
+	fmt.Printf("email from    : %#v\n", email.Headers.From[0])
+	fmt.Printf("email subject : %s\n", email.Headers.Subject)
+	fmt.Printf("email text    : %s", email.Text)
+	fmt.Printf("email err     : %t \n", err != nil)
+
+	if got, want := email.Headers.From[0].Address, "joe@bloggs.com"; got != want {
+		t.Errorf("Address.Address got %s want %s", got, want)
+	}
+
+	tpl := `parser
+	parser:              %s
+	parseAddressHeader:  %T
+	parseDateHeaderFunc: %T
+	parserFunc:          %T
+`
+	pT := ""
+	switch parser.parser {
+	case wholeEmail:
+		pT = "wholeEmail"
+	case headersOnly:
+		pT = "headersOnly"
+	case skipAttachments:
+		pT = "skipAttachments"
+	}
+	fmt.Printf(tpl, pT, parser.parseAddressHeader, parser.parseDateHeaderFunc, parser.parserFunc)
+
+	if got, want := pT, "skipAttachments"; got != want {
+		t.Errorf("parser mode got %s want %s", got, want)
+	}
 
 	if err != nil {
 		t.Fatal(err)
